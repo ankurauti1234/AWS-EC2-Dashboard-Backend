9665e5fe75e522df7c08abc4842672330e9ead87
Ankur Auti <ankurauti@gmail.com>
2024-05-09 14:29:23 +0530

    Alerts addedd

--- index.js
+++ index.js
@@ -5,7 +5,29 @@ const { faker } = require("@faker-js/faker");
 
 mongoConnect();
 
+const dominicanRepublicLocations = [
+  { lat: 18.4861, lon: -69.9312 }, // Santo Domingo
+  { lat: 19.7808, lon: -70.6871 }, // Santiago
+  { lat: 18.4274, lon: -68.9728 }, // Punta Cana
+  { lat: 19.0502, lon: -70.1496 }, // Puerto Plata
+  { lat: 18.809, lon: -69.811 }, // La Romana
+  { lat: 18.7357, lon: -70.1627 }, // San Pedro de Macorís
+  { lat: 19.5513, lon: -71.0758 }, // Mao
+  { lat: 19.217, lon: -69.336 }, // Nagua
+  { lat: 19.6122, lon: -71.2186 }, // Monte Cristi
+  { lat: 18.5885, lon: -68.4053 }, // Higüey
+];
+
+
+
+
 function generateRandomData() {
+
+  const randomDominicanRepublicLocation =
+    dominicanRepublicLocations[
+      Math.floor(Math.random() * dominicanRepublicLocations.length)
+    ];
+
   return {
     Watermark_id: {
       reporting_time: Date.now(),
@@ -23,8 +45,8 @@ function generateRandomData() {
     },
     Location: {
       Cell_Info: {
-        lat: 18.4861,
-        lon: -69.9312,
+        lat: randomDominicanRepublicLocation.lat,
+        lon: randomDominicanRepublicLocation.lon,
       },
       Installing: faker.datatype.boolean(),
     },
@@ -40,9 +62,75 @@ function generateRandomData() {
   };
 }
 
-async function saveDataToMongo(inputJSON) {
+function generateRandomAlertData() {
+  const eventType = faker.number.int({ min: 1, max: 5 }); // Generate a random event type
+
+  switch (eventType) {
+    case 1: // TAMPER_ALARM
+      return {
+        ID: faker.number.int(),
+        TS: Date.now(),
+        Type: 1,
+        Details: {
+          Meter_tamper: faker.datatype.boolean(),
+          Tv_plug_tamper: faker.datatype.boolean(),
+          Tamper_ts: Date.now(),
+        },
+      };
+
+    case 2: // SOS_ALARM
+      return {
+        ID: faker.number.int(),
+        TS: Date.now(),
+        Type: 2,
+        Details: { sos: true },
+      };
+
+    case 3: // BATTERY_ALARM
+      return {
+        ID: faker.number.int(),
+        TS: Date.now(),
+        Type: 3,
+        Details: {
+          main_bat_fail: faker.datatype.boolean(),
+          rtc_fail: faker.datatype.boolean(),
+          rtc_bat_low: faker.datatype.boolean(),
+        },
+      };
+
+    case 4: // SIM_ALERT
+      return {
+        ID: faker.number.int(),
+        TS: Date.now(),
+        Type: 4,
+        Details: {
+          sim1_absent: faker.datatype.boolean(),
+          sim1_changed: faker.datatype.boolean(),
+          sim2_absent: faker.datatype.boolean(),
+          sim2_changed: faker.datatype.boolean(),
+        },
+      };
+
+    case 5: // SYSTEM_ALARM
+      return {
+        ID: faker.number.int(),
+        TS: Date.now(),
+        Type: 5,
+        Details: {
+          name: "System Error",
+          error_code: faker.number.int({ min: 400, max: 599 }),
+          message: faker.lorem.words(),
+        },
+      };
+
+    default:
+      return null;
+  }
+}
+
+async function saveDataToMongo(inputJSON, schema) {
   try {
-    const result = await deviceSchema.insertMany(inputJSON);
+    const result = await schema.insertMany(inputJSON);
     if (result.length > 0) {
       return {
         statusCode: 200,
@@ -56,18 +144,32 @@ async function saveDataToMongo(inputJSON) {
   }
 }
 
-function sendDataToMongoEvery3Hours() {
+function sendDataToMongoEvery5sec() {
   setInterval(() => {
     const dummyData = generateRandomData();
-    saveDataToMongo([dummyData])
+    saveDataToMongo([dummyData], deviceSchema)
       .then((result) => {
         console.log(result.message);
       })
       .catch((error) => {
         console.error(error.message);
       });
-  }, 5000); // 3 hours in milliseconds
+  }, 5000); // 5 seconds in milliseconds
 }
 
-// Call the function to start sending data
-sendDataToMongoEvery3Hours();
+function sendDataToMongoEvery5min() {
+  setInterval(() => {
+    const dummyData = generateRandomAlertData();
+    saveDataToMongo([dummyData], deviceAlertsSchema)
+      .then((result) => {
+        console.log(result.message, "alert");
+      })
+      .catch((error) => {
+        console.error(error.message);
+      });
+  }, 5 * 60000); // 5 minute in milliseconds
+}
+
+// Call the functions to start sending data
+sendDataToMongoEvery5sec();
+sendDataToMongoEvery5min();
--- model/deviceAlertsModel.js
+++ model/deviceAlertsModel.js
@@ -1,34 +1,10 @@
 const mongoose = require("mongoose");
 
 const devAlertSchema = new mongoose.Schema({
-  Tamper_Alarm: {
-    Meter_tamper: { type: Boolean },
-    Tv_plug_tamper: { type: Boolean },
-    Tamper_ts: { type: Number },
-  },
-  SOS_Alarm: {
-    SOS: { type: Boolean },
-  },
-  Battery_Alarm: {
-    Main_bat_fail: { type: Boolean },
-    Rtc_fail: { type: Boolean },
-    Rtc_bat_low: { type: Boolean },
-  },
-  Power_Alarm: {
-    Power_supply_bad: { type: Boolean },
-    Power_supply_fail: { type: Boolean },
-  },
-
-  Sim_Alert: {
-    Sim1_Absent: { type: Boolean },
-    Sim1_Changed: { type: Boolean },
-    Sim2_Absent: { type: Boolean },
-    Sim2_Changed: { type: Boolean },
-  },
-  System_Alarm: {
-    Name: { type: String }, // name of alarm
-    Details: { type: String }, // Must be valid JSON
-  },
+  ID: { type: Number, required: true }, // Event type
+  Type: { type: Number, required: true }, // Event type
+  TS: { type: Number, required: true }, // Timestamp of the alert
+  Details: { type: mongoose.Schema.Types.Mixed }, // Details of the alert
 });
 
 const deviceAlertsSchema = mongoose.model(
