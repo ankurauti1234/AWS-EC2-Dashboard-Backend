1cfede9380ee033d50f2b1476550dda67ca9c829
Ankur Auti <ankurauti@gmail.com>
2024-05-17 15:41:51 +0530

    modified code  for expected output

--- index.js
+++ index.js
@@ -18,55 +18,334 @@ const dominicanRepublicLocations = [
   { lat: 18.5885, lon: -68.4053 }, // Higüey
 ];
 
+let lastDeviceId = 0;
+let lastAlertId = 0;
+
 function generateRandomData() {
+
+    lastDeviceId++;
+
   const randomDominicanRepublicLocation =
     dominicanRepublicLocations[
       Math.floor(Math.random() * dominicanRepublicLocations.length)
     ];
 
   return {
-    Watermark_id: {
-      reporting_time: Date.now(),
-      id: 69,
-      confidence: faker.number.int({ min: 0, max: 100 }),
-    },
-    Meter_Installation: {
-      HHID: "APM32",
-      Success: faker.datatype.boolean(),
-    },
-    Power: {
-      Tv: faker.datatype.boolean(),
-      Main: faker.datatype.boolean(),
-      Smps: faker.datatype.boolean(),
-    },
-    Location: {
+    device: faker.number.int({ min: 1, max: 5 }),
+
+    LOCATION: {
+      ID: lastDeviceId,
+      TS: Date.now(),
+      Type: 1,
       Cell_Info: {
         lat: randomDominicanRepublicLocation.lat,
         lon: randomDominicanRepublicLocation.lon,
       },
       Installing: faker.datatype.boolean(),
     },
-    Network_Latch: {
+
+    GUEST_REGISTRATION: {
+      ID: lastDeviceId,
+      TS: Date.now(),
+      Type: 2,
+      guest_id: faker.number.int({ min: 1, max: 6 }),
+      registering: faker.datatype.boolean(),
+      guest_age: faker.number.int({ min: 6, max: 80 }),
+      guest_male: faker.datatype.boolean(),
+    },
+
+    MEMBER_GUEST_DECLARATION: {
+      ID: lastDeviceId,
+      TS: Date.now(),
+      Type: 3,
+      member_keys: [
+        faker.datatype.boolean(),
+        faker.datatype.boolean(),
+        faker.datatype.boolean(),
+        faker.datatype.boolean(),
+        faker.datatype.boolean(),
+        faker.datatype.boolean(),
+        faker.datatype.boolean(),
+        faker.datatype.boolean(),
+        faker.datatype.boolean(),
+        faker.datatype.boolean(),
+        faker.datatype.boolean(),
+        faker.datatype.boolean(),
+      ],
+      guests: [
+        faker.datatype.boolean(),
+        faker.datatype.boolean(),
+        faker.datatype.boolean(),
+        faker.datatype.boolean(),
+        faker.datatype.boolean(),
+      ],
+      confidence: 80,
+    },
+    FINGERPRINT_ID: {
+      ID: lastDeviceId,
+      TS: Date.now(),
+      Type: 4,
+      reporting_time: Date.now(),
+      id: faker.number.int({ min: 1, max: 50 }),
+      confidence: faker.number.int({ min: 0, max: 100 }),
+    },
+
+    FINGERPRINT_TS: {
+      ID: lastDeviceId,
+      TS: Date.now(),
+      Type: 5,
+      reporting_time: Date.now(),
+      embedded_time: Date.now(),
+      confidence: faker.number.int({ min: 0, max: 100 }),
+      offset: 256,
+    },
+
+    CONFIGURATION: {
+      ID: lastDeviceId,
+      TS: Date.now(),
+      Type: 8,
+      differential_mode: faker.datatype.boolean(),
+      member_keys: [
+        faker.datatype.boolean(),
+        faker.datatype.boolean(),
+        faker.datatype.boolean(),
+        faker.datatype.boolean(),
+        faker.datatype.boolean(),
+        faker.datatype.boolean(),
+        faker.datatype.boolean(),
+        faker.datatype.boolean(),
+        faker.datatype.boolean(),
+        faker.datatype.boolean(),
+        faker.datatype.boolean(),
+        faker.datatype.boolean(),
+      ],
+      guest_cancellation_time: 300,
+      software_version: faker.system.semver(),
+      power_pcb_firmware_version: faker.system.semver(),
+      remote_firmware_version: faker.system.semver(),
+      audio_configuration: [
+        faker.datatype.boolean(),
+        faker.datatype.boolean(),
+        faker.datatype.boolean(),
+        faker.datatype.boolean(),
+        faker.datatype.boolean(),
+      ],
+      audience_day_start_time: 600,
+      no_of_sessions: 8,
+    },
+
+    METER_INSTALLATION: {
+      ID: lastDeviceId,
+      TS: Date.now(),
+      Type: 15,
+      HHID: "APM32",
+      Success: faker.datatype.boolean(),
+    },
+
+    VOLTAGE_STATS: {
+      ID: lastDeviceId,
+      TS: Date.now(),
+      Type: 18,
+      high_rail_voltage: 3000,
+      mid_rail_voltage: 1500,
+      gsm_rail_voltage: 3300,
+      rtc_battery_voltage: 3200,
+      li_ion_battery_voltage: 3800,
+      remote_battery_voltage: 2500,
+    },
+
+    TEMPERATURE_STATS: {
+      ID: lastDeviceId,
+      TS: Date.now(),
+      Type: 19,
+      battery_temp: 25,
+      arm_core_temp: 30,
+      power_pcb_temp: 28,
+      rtc_temp: 27,
+    },
+
+    NTP_SYNC: {
+      ID: lastDeviceId,
+      TS: Date.now(),
+      Type: 20,
+      server: "time.google.com",
+      system_time: Date.now(),
+      success: faker.datatype.boolean(),
+      error_code: 0,
+      drift: 5,
+      jumped: faker.datatype.boolean(),
+    },
+
+    AUDIENCE_SESSION_CLOSE: {
+      ID: lastDeviceId,
+      TS: Date.now(),
+      Type: 21,
+      stop_time: 930,
+      viewing_member_keys: [
+        faker.datatype.boolean(),
+        faker.datatype.boolean(),
+        faker.datatype.boolean(),
+      ],
+      viewing_guests: [
+        faker.datatype.boolean(),
+        faker.datatype.boolean(),
+        faker.datatype.boolean(),
+      ],
+      tv_on: faker.datatype.boolean(),
+      last_watermark_id: 123,
+      tv_event_ts: 456,
+      last_watermark_id_ts: Date.now(),
+      marked: 80,
+    },
+
+    NETWORK_LATCH: {
+      ID: lastDeviceId,
+      TS: Date.now(),
+      Type: 23,
       Ip_up: faker.datatype.boolean(),
       Sim: faker.number.int({ min: 1, max: 2 }),
     },
+
+    REMOTE_PAIRING: {
+      ID: lastDeviceId,
+      TS: Date.now(),
+      Type: 24,
+      remote_id: 123456789,
+      success: faker.datatype.boolean(),
+    },
+
+    REMOTE_ACTIVITY: {
+      ID: lastDeviceId,
+      TS: Date.now(),
+      Type: 25,
+      lock: faker.datatype.boolean(),
+      orr: faker.datatype.boolean(),
+      absent_key_press: faker.datatype.boolean(),
+      drop: faker.datatype.boolean(),
+    },
+
+    SYSTEM_INFO: {
+      ID: lastDeviceId,
+      TS: Date.now(),
+      Type: 28,
+      rpi_serial: "1234567890",
+      pcb_serial: "PCB123",
+      imei: "123456789012345",
+      imsi_1: "123456789012345",
+      imsi_2: "123456789012345",
+      eeprom: 987654321,
+      wifi_serial: "WIFI123",
+      mac_serial: "MAC123",
+      remote_serial: 13579,
+    },
+
+    CONFIG_UPDATE: {
+      ID: lastDeviceId,
+      TS: Date.now(),
+      Type: 29,
+      key: "timeout",
+      value: "60",
+      old_value: "30",
+    },
+
+    ALIVE: {
+      ID: lastDeviceId,
+      TS: Date.now(),
+      Type: 30,
+      state: faker.datatype.boolean(),
+    },
+
     METER_OTA: {
+      ID: lastDeviceId,
+      TS: Date.now(),
+      Type: 31,
       previous: faker.system.semver(),
       update: faker.system.semver(),
       success: faker.datatype.boolean(),
+    },
+
+    POWER: {
+      ID: lastDeviceId,
+      TS: Date.now(),
+      Type: 32,
+      Tv: faker.datatype.boolean(),
+      Main: faker.datatype.boolean(),
+      Smps: faker.datatype.boolean(),
+    },
+
+    BATTERY_VOLTAGE: {
+      ID: lastDeviceId,
+      TS: Date.now(),
+      Type: 33,
+      Rtc: 2882,
+      Meter: 4164,
+    },
+
+    BOOT: {
+      ID: lastDeviceId,
+      TS: Date.now(),
+      Type: 35,
+      boot_ts: Date.now(),
+      last_boot_ts: Date.now(),
+      last_shutdown_ts: Date.now(),
+      clean: faker.datatype.boolean(),
+    },
+
+    INSTALL_READY: {
+      ID: lastDeviceId,
+      TS: Date.now(),
+      Type: 36,
+      hhid: 123456789,
+      remote_id: 987654321,
+      sim1_imsi: "123456789012345",
+      sim2_imsi: "543210987654321",
+      sim1_pass: faker.datatype.boolean(),
+      sim2_pass: faker.datatype.boolean(),
+    },
+
+    BOOT_V2: {
+      ID: lastDeviceId,
+      TS: Date.now(),
+      Type: 37,
+      rtc_ts: Date.now(),
+      ntp_ts: Date.now(),
+      cell_ts: Date.now(),
+      boot_ts: Date.now(),
+      clean: faker.datatype.boolean(),
+      time_approximated: faker.datatype.boolean(),
+      events_ignored: faker.datatype.boolean(),
+    },
+    STB: {
+      ID: lastDeviceId,
+      TS: Date.now(),
+      Type: 38,
+      state: faker.datatype.boolean(),
+    },
+    DERIVED_TV_STATUS: {
+      ID: lastDeviceId,
+      TS: Date.now(),
+      Type: 39,
+      state: faker.datatype.boolean(),
+    },
+    AUDIO_SOURCE: {
+      ID: lastDeviceId,
+      TS: Date.now(),
+      Type: 40,
+      Status: "line_in",
     },
   };
 }
 
 function generateRandomAlertData() {
+  lastAlertId++
   const eventType = faker.number.int({ min: 1, max: 5 }); // Generate a random event type
 
   switch (eventType) {
     case 1: // TAMPER_ALARM
       return {
-        ID: faker.number.int(),
+        ID: lastAlertId,
         TS: Date.now(),
-        Type: 1,
+        Type: 9,
         Details: {
           Meter_tamper: faker.datatype.boolean(),
           Tv_plug_tamper: faker.datatype.boolean(),
@@ -76,17 +355,17 @@ function generateRandomAlertData() {
 
     case 2: // SOS_ALARM
       return {
-        ID: faker.number.int(),
+        ID: lastAlertId,
         TS: Date.now(),
-        Type: 2,
-        Details: { sos: true },
+        Type: 10,
+        Details: { sos: faker.datatype.boolean() },
       };
 
     case 3: // BATTERY_ALARM
       return {
-        ID: faker.number.int(),
+        ID: lastAlertId,
         TS: Date.now(),
-        Type: 3,
+        Type: 11,
         Details: {
           main_bat_fail: faker.datatype.boolean(),
           rtc_fail: faker.datatype.boolean(),
@@ -96,9 +375,9 @@ function generateRandomAlertData() {
 
     case 4: // SIM_ALERT
       return {
-        ID: faker.number.int(),
+        ID: lastAlertId,
         TS: Date.now(),
-        Type: 4,
+        Type: 26,
         Details: {
           sim1_absent: faker.datatype.boolean(),
           sim1_changed: faker.datatype.boolean(),
@@ -109,13 +388,13 @@ function generateRandomAlertData() {
 
     case 5: // SYSTEM_ALARM
       return {
-        ID: faker.number.int(),
+        ID: lastAlertId,
         TS: Date.now(),
-        Type: 5,
+        Type: 27,
         Details: {
           name: "System Error",
           error_code: faker.number.int({ min: 400, max: 599 }),
-          message: faker.lorem.words({ min: 8, max: 15 }),
+          message: faker.lorem.words({ min: 5, max: 10 }),
         },
       };
 
--- model/deviceModel.js
+++ model/deviceModel.js
@@ -1,24 +1,12 @@
 const mongoose = require("mongoose");
 
 const devSchema = new mongoose.Schema({
-  Watermark_id: {
-    reporting_time: { type: Number, required: true },
-    id: { type: Number, required: true },
-    confidence: { type: Number, required: true, default: 0, min: 0, max: 100 },
-  },
-
-  Meter_Installation: {
-    HHID: { type: String, required: true },
-    Success: { type: Boolean, required: true, default: false },
-  },
-
-  Power: {
-    Tv: { type: Boolean, required: true, default: true },
-    Main: { type: Boolean, required: true, default: true },
-    Smps: { type: Boolean, required: true, default: true },
-  },
+  device: { type: Number, required: true },
 
   Location: {
+    ID: { type: Number, required: true },
+    TS: { type: Number, required: true },
+    Type: { type: Number, required: true },
     Cell_Info: {
       lat: { type: Number, required: true },
       lon: { type: Number, required: true },
@@ -26,18 +14,230 @@ const devSchema = new mongoose.Schema({
     Installing: { type: Boolean, required: true, default: true },
   },
 
+  GUEST_REGISTRATION: {
+    ID: { type: Number, required: true },
+    TS: { type: Number, required: true },
+    Type: { type: Number, required: true },
+    guest_id: { type: Number, required: true },
+    registering: { type: Boolean, required: true },
+    guest_age: { type: Number, required: true },
+    guest_male: { type: Boolean, required: true },
+  },
+
+  MEMBER_GUEST_DECLARATION: {
+    ID: { type: Number, required: true },
+    TS: { type: Number, required: true },
+    Type: { type: Number, required: true },
+    member_keys: [{ type: Boolean, required: true }],
+    guests: [{ type: Boolean, required: true }],
+    confidence: { type: Number, required: true },
+  },
+
+  Fingerprint_id: {
+    ID: { type: Number, required: true },
+    TS: { type: Number, required: true },
+    Type: { type: Number, required: true },
+    reporting_time: { type: Number, required: true },
+    id: { type: Number, required: true },
+    confidence: { type: Number, required: true },
+  },
+
+  FINGERPRINT_TS: {
+    ID: { type: Number, required: true },
+    TS: { type: Number, required: true },
+    Type: { type: Number, required: true },
+    reporting_time: { type: Number, required: true },
+    embedded_time: { type: Number, required: true },
+    confidence: { type: Number, required: true },
+    offset: { type: Number, required: true },
+  },
+
+  CONFIGURATION: {
+    ID: { type: Number, required: true },
+    TS: { type: Number, required: true },
+    Type: { type: Number, required: true },
+    differential_mode: { type: Boolean, required: true },
+    member_keys: [{ type: Boolean, required: true }],
+    guest_cancellation_time: { type: Number, required: true },
+    software_version: { type: String, required: true },
+    power_pcb_firmware_version: { type: String, required: true },
+    remote_firmware_version: { type: String, required: true },
+    audio_configuration: [{ type: Boolean, required: true }],
+    audience_day_start_time: { type: Number, required: true },
+    no_of_sessions: { type: Number, required: true },
+  },
+
+  Meter_Installation: {
+    ID: { type: Number, required: true },
+    TS: { type: Number, required: true },
+    Type: { type: Number, required: true },
+    HHID: { type: String, required: true },
+    Success: { type: Boolean, required: true },
+  },
+
+  VOLTAGE_STATS: {
+    ID: { type: Number, required: true },
+    TS: { type: Number, required: true },
+    Type: { type: Number, required: true },
+    high_rail_voltage: { type: Number, required: true },
+    mid_rail_voltage: { type: Number, required: true },
+    gsm_rail_voltage: { type: Number, required: true },
+    rtc_battery_voltage: { type: Number, required: true },
+    li_ion_battery_voltage: { type: Number, required: true },
+    remote_battery_voltage: { type: Number, required: true },
+  },
+
+  TEMPERATURE_STATS: {
+    ID: { type: Number, required: true },
+    TS: { type: Number, required: true },
+    Type: { type: Number, required: true },
+    battery_temp: { type: Number, required: true },
+    arm_core_temp: { type: Number, required: true },
+    power_pcb_temp: { type: Number, required: true },
+    rtc_temp: { type: Number, required: true },
+  },
+
+  NTP_SYNC: {
+    ID: { type: Number, required: true },
+    TS: { type: Number, required: true },
+    Type: { type: Number, required: true },
+    server: { type: String, required: true },
+    system_time: { type: Number, required: true },
+    success: { type: Boolean, required: true },
+    error_code: { type: Number, required: true },
+    drift: { type: Number, required: true },
+    jumped: { type: Boolean, required: true },
+  },
+
+  AUDIENCE_SESSION_CLOSE: {
+    ID: { type: Number, required: true },
+    TS: { type: Number, required: true },
+    Type: { type: Number, required: true },
+    stop_time: { type: Number, required: true },
+    viewing_member_keys: [{ type: Boolean, required: true }],
+    viewing_guests: [{ type: Boolean, required: true }],
+    tv_on: { type: Boolean, required: true },
+    last_watermark_id: { type: Number, required: true },
+    tv_event_ts: { type: Number, required: true },
+    last_watermark_id_ts: { type: Number, required: true },
+    marked: { type: Number, required: true },
+  },
+
   Network_Latch: {
+    ID: { type: Number, required: true },
+    TS: { type: Number, required: true },
+    Type: { type: Number, required: true },
     Ip_up: { type: Boolean, required: true },
     Sim: { type: Number, required: true }, // 1 for sim1 2 for sim2
   },
 
+  REMOTE_PAIRING: {
+    ID: { type: Number, required: true },
+    TS: { type: Number, required: true },
+    Type: { type: Number, required: true },
+    remote_id: { type: Number, required: true },
+    success: { type: Boolean, required: true },
+  },
+
+  REMOTE_ACTIVITY: {
+    ID: { type: Number, required: true },
+    TS: { type: Number, required: true },
+    Type: { type: Number, required: true },
+    lock: { type: Boolean, required: true },
+    orr: { type: Boolean, required: true },
+    absent_key_press: { type: Boolean, required: true },
+    drop: { type: Boolean, required: true },
+  },
+
+  SYSTEM_INFO: {
+    ID: { type: Number, required: true },
+    TS: { type: Number, required: true },
+    Type: { type: Number, required: true },
+    rpi_serial: { type: String, required: true },
+    pcb_serial: { type: String, required: true },
+    imei: { type: String, required: true },
+    imsi_1: { type: String, required: true },
+    imsi_2: { type: String, required: true },
+    eeprom: { type: Number, required: true },
+    wifi_serial: { type: String, required: true },
+    mac_serial: { type: String, required: true },
+    remote_serial: { type: Number, required: true },
+  },
+
+  CONFIG_UPDATE: {
+    ID: { type: Number, required: true },
+    TS: { type: Number, required: true },
+    Type: { type: Number, required: true },
+    key: { type: String, required: true },
+    value: { type: String, required: true },
+    old_value: { type: String, required: true },
+  },
+
+  ALIVE: {
+    ID: { type: Number, required: true },
+    TS: { type: Number, required: true },
+    Type: { type: Number, required: true },
+    state: { type: Boolean, required: true },
+  },
+
   METER_OTA: {
+    ID: { type: Number, required: true },
+    TS: { type: Number, required: true },
+    Type: { type: Number, required: true },
     previous: { type: String, required: true },
     update: { type: String, required: true },
     success: { type: Boolean, required: true },
   },
+
+  Power: {
+    Tv: { type: Boolean, required: true },
+    Main: { type: Boolean, required: true },
+    Smps: { type: Boolean, required: true },
+  },
+
+  BATTERY_VOLTAGE: {
+    ID: { type: Number, required: true },
+    TS: { type: Number, required: true },
+    Type: { type: Number, required: true },
+    Rtc: { type: Number, required: true },
+    Meter: { type: Number, required: true },
+  },
+
+  BOOT: {
+    ID: { type: Number, required: true },
+    TS: { type: Number, required: true },
+    Type: { type: Number, required: true },
+    boot_ts: { type: Number },
+    install: { type: Number},
+    last_boot: { type: Number },
+    relay_status: [{ type: Boolean }],
+  },
+
+  BOOT_V2: {
+    ID: { type: Number, required: true },
+    TS: { type: Number, required: true },
+    Type: { type: Number, required: true },
+    boot_ts: { type: Number},
+    install: { type: Number },
+    last_boot: { type: Number },
+    relay_status: [{ type: Boolean }],
+  },
+
+  STB_DERIVED_TV_STATUS: {
+    ID: { type: Number },
+    TS: { type: Number},
+    Type: { type: Number},
+    derived: { type: Boolean, },
+  },
+
+  AUDIO_SOURCE: {
+    ID: { type: Number, required: true },
+    TS: { type: Number, required: true },
+    Type: { type: Number, required: true },
+    source: { type: String },
+  },
 });
 
-const deviceSchema = mongoose.model("device", devSchema, "iot_device_info");
+const Device = mongoose.model("deviceInfo", devSchema, "iot_device_info");
 
-module.exports = deviceSchema;
+module.exports = Device;
